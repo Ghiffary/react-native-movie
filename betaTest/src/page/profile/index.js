ce272c09085dc9c1c1fc5cd1bfa48d9daec9d1dd
Ghiffary <rezza993@gmail.com>
2019-06-25 18:50:21 +0700

    [Ghiffary] Update Profile

--- betaTest/src/page/profile/index.js
+++ betaTest/src/page/profile/index.js
@@ -1,25 +1,119 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
-import Icon from "react-native-vector-icons/Ionicons";
+// import React from 'react';
+// import { Text, View } from 'react-native';
+// import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+// import Icon from "react-native-vector-icons/Ionicons";
 
 
-class ProfileScreen extends React.Component {
+// class ProfileScreen extends React.Component {
+//   render() {
+//     return (
+//       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//         <Text>Profile!</Text>
+//         <Icon
+//   name="md-add"
+//   color="#ccc"
+//   size={25}
+// />
+//       </View>
+      
+//     );
+//   }
+// }
+
+
+
+// export default createAppContainer(ProfileScreen);
+
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity
+} from 'react-native';
+
+export default class Profile extends Component {
+
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Profile!</Text>
-        <Icon
-  name="md-add"
-  color="#ccc"
-  size={25}
-/>
+      <View style={styles.container}>
+          <View style={styles.header}></View>
+          <Image style={styles.avatar} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
+          <View style={styles.body}>
+            <View style={styles.bodyContent}>
+              <Text style={styles.name}>Ghiffary</Text>
+              <Text style={styles.info}>UX and UI Designer / Mobile developer</Text>
+              <Text style={styles.description}>Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne assum electram expetendis, omittam deseruisse consequuntur ius an,</Text>
+              
+              <TouchableOpacity style={styles.buttonContainer}>
+                <Text>Opcion 1</Text>  
+              </TouchableOpacity>              
+              <TouchableOpacity style={styles.buttonContainer}>
+                <Text>Opcion 2</Text> 
+              </TouchableOpacity>
+            </View>
+        </View>
       </View>
-      
     );
   }
 }
 
-
-
-export default createAppContainer(ProfileScreen);
+const styles = StyleSheet.create({
+  header:{
+    backgroundColor: "#00BFFF",
+    height:200,
+  },
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: "white",
+    marginBottom:10,
+    alignSelf:'center',
+    position: 'absolute',
+    marginTop:130
+  },
+  name:{
+    fontSize:22,
+    color:"#FFFFFF",
+    fontWeight:'600',
+  },
+  body:{
+    marginTop:40,
+  },
+  bodyContent: {
+    flex: 1,
+    alignItems: 'center',
+    padding:30,
+  },
+  name:{
+    fontSize:28,
+    color: "#696969",
+    fontWeight: "600"
+  },
+  info:{
+    fontSize:16,
+    color: "#00BFFF",
+    marginTop:10
+  },
+  description:{
+    fontSize:16,
+    color: "#696969",
+    marginTop:10,
+    textAlign: 'center'
+  },
+  buttonContainer: {
+    marginTop:10,
+    height:45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    width:250,
+    borderRadius:30,
+    backgroundColor: "#00BFFF",
+  },
+});
